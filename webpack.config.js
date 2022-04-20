e05b2ab903ed746cdf5a3af932b891cd11dfca28
Александр <zbc@mail.ru>
2022-04-20 21:00:44 +0300

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,14 @@ module.exports = {
         print: './src/print.js',
     },
     devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        client: {
+            overlay: true,
+            progress: true,
+            reconnect: true,
+        },
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Development',
@@ -18,5 +26,8 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
     },
 };
