e64a1b3257afe81ab8d94989ac390c9a2a87eb04
Александр <zbc@mail.ru>
2022-04-21 16:04:04 +0300

    typescript config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,42 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        client: {
+            overlay: true,
+            progress: true,
+            reconnect: true,
+        },
+    },
+    entry: './src/index.ts',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Caching',
+            // minify: true,
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
     },
 };
